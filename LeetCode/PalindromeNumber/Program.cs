a26aa8e3f8393f2a85eaba65317454b14e3b7c6b
Aleksandr Rakushev <alexandr.rakushev@akvelon.com>
2019-07-30 21:54:02 +0300

    * added IsPalindrome decision https://leetcode.com/problems/palindrome-number/

---
+++ LeetCode/PalindromeNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var sln = new Solution();
+            Console.WriteLine(sln.IsPalindrome(12312));
+        }
+    }
+}
---
+++ LeetCode/PalindromeNumber/Solution.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace PalindromeNumber
+{    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 10 && x >= 0)
+                return true;
+            if (x < 0 || x % 10 == 0)
+                return false;
+
+            int revertedNumber = 0;
+            while (x > revertedNumber)
+            {
+                revertedNumber = revertedNumber * 10 + x % 10;
+                x /= 10;
+            }
+
+            return x == revertedNumber || x == revertedNumber / 10;
+        }
+    }
+}
