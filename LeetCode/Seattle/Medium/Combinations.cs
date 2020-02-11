7358010a3fb01738c1391287d4e570b4aea203d0
Aleksandr Rakushev <alexvmk87@mail.ru>
2020-02-10 16:52:39 -0800

    + Combinations

--- LeetCode/Seattle/Medium/Combinations.cs
+++ LeetCode/Seattle/Medium/Combinations.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -10,51 +11,104 @@ namespace Seattle.Medium
     /// <summary>
     /// Combinations.
     /// https://leetcode.com/problems/combinations/ .
+    /// Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
     /// </summary>
     public class Combinations
     {
         public IList<IList<int>> Combine(int n, int k)
         {
             var result = new List<IList<int>>();
+            if (n == 0 || k == 0 || k > n) return result;
+
+            BackTrack(1, result, new List<int>(), n, k);
             return result;
         }
 
-        private void BackTrack(List<string> resList, string curStr, int open, int close, int pairsNumb)
+        private void BackTrack(int i, List<IList<int>> result, List<int> comb, int n, int k)
         {
             // end of the execution stack and add result into output list:
-            if (curStr.Length == pairsNumb * 2)
+            if (comb.Count() == k)
             {
-                resList.Add(curStr);
+                result.Add(comb.ToList());
                 return;
             }
 
-            // check the number of open parentheses that it no more then their max count:
-            if (open < pairsNumb)
+            for (int j = i; j <= n; j++)
             {
-                // backtrack it with "("
-                BackTrack(resList, curStr + "(", open + 1, close, pairsNumb);
-            }
-
-            // check the number of close parentheses that it no more then open count:
-            if (close < open)
-            {
-                BackTrack(resList, curStr + ")", open, close + 1, pairsNumb);
+                comb.Add(j);
+                BackTrack(j + 1, result, comb, n, k);
+                comb.RemoveAt(comb.Count - 1);
             }
         }
     }
 
     [Collection(SeattleCollectionFixture.SeattleCollectionFixtureName)]
     public class CombinationsTests
-        {
+    {
         public CombinationsTests(SeattleTestsFixture seattleTestsFixture)
         {
         }
 
-        [Theory]
-        [InlineData(4, 2)]
-        public void Combine(int n, int k)
+        [Fact]
+        public void Combine()
         {
             var algorithm = new Combinations();
+            var res = algorithm.Combine(0, 0);
+            Assert.Empty(res);
+
+            res = algorithm.Combine(1, 0);
+            Assert.Empty(res);
+
+            res = algorithm.Combine(0, 1);
+            Assert.Empty(res);
+
+            res = algorithm.Combine(1, 2);
+            Assert.Empty(res);
+
+            // output: [ [1], [2] ]
+            res = algorithm.Combine(2, 1);
+            Assert.Equal(2, res.Count);
+            Assert.Equal(1, res[0].Count);
+            Assert.Contains(1, res[0]);
+            Assert.Equal(1, res[1].Count);
+            Assert.Contains(2, res[1]);
+
+            // output:
+            // Input: n = 4, k = 2
+            // Output:
+            // [
+            //   [1,2],
+            //   [1,3],
+            //   [1,4],
+            //   [2,3],
+            //   [2,4],
+            //   [3,4],
+            // ]
+            res = algorithm.Combine(4, 2);
+            Assert.Equal(6, res.Count);
+            Assert.Equal(2, res[0].Count);
+            Assert.Contains(1, res[0]);
+            Assert.Contains(2, res[0]);
+
+            Assert.Equal(2, res[1].Count);
+            Assert.Contains(1, res[1]);
+            Assert.Contains(3, res[1]);
+
+            Assert.Equal(2, res[2].Count);
+            Assert.Contains(1, res[2]);
+            Assert.Contains(4, res[2]);
+
+            Assert.Equal(2, res[3].Count);
+            Assert.Contains(2, res[3]);
+            Assert.Contains(3, res[3]);
+
+            Assert.Equal(2, res[4].Count);
+            Assert.Contains(2, res[4]);
+            Assert.Contains(4, res[4]);
+
+            Assert.Equal(2, res[2].Count);
+            Assert.Contains(3, res[5]);
+            Assert.Contains(4, res[5]);
         }
     }
 }
