f203636eb51e998e532dc90fd68a62bdd08867d4
Aleksandr Rakushev <alexvmk87@mail.ru>
2020-02-24 13:53:01 -0800

    * ValidPalindrome

--- LeetCode/Seattle/Easy/ValidPalindrome.cs
+++ LeetCode/Seattle/Easy/ValidPalindrome.cs
@@ -13,6 +13,40 @@ namespace Seattle.Easy
     /// </summary>
     public class ValidPalindrome
     {
+        public bool IsPalindrome(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s) || s.Length == 1)
+            {
+                return true;
+            }
+
+            int leftPointer = 0;
+            int rigthPointer = s.Length - 1;
+
+            while (leftPointer <= rigthPointer)
+            {
+                if (!char.IsLetterOrDigit(s[leftPointer]))
+                {
+                    leftPointer++;
+                }
+                else if (!char.IsLetterOrDigit(s[rigthPointer]))
+                {
+                    rigthPointer--;
+                }
+                else
+                {
+                    if (char.ToLower(s[leftPointer]) != char.ToLower(s[rigthPointer]))
+                    {
+                        return false;
+                    }
+
+                    leftPointer++;
+                    rigthPointer--;
+                }
+            }
+
+            return true;
+        }
     }
 
     [Collection(SeattleCollectionFixture.SeattleCollectionFixtureName)]
@@ -25,6 +59,14 @@ namespace Seattle.Easy
         [Fact]
         public void ValidPalindrome()
         {
+            var alg = new ValidPalindrome();
+            Assert.True(alg.IsPalindrome("A man, a plan, a canal: Panama"));
+            Assert.False(alg.IsPalindrome("race a car"));
+
+            Assert.True(alg.IsPalindrome(" r"));
+
+            Assert.True(alg.IsPalindrome("race ecar"));
+            Assert.True(alg.IsPalindrome("234  33 3 3  333432"));
         }
     }
 }
