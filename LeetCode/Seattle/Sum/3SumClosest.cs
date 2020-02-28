a09d60edef5807133b1f8d9a0fd8c269f3104dbb
Aleksandr Rakushev <alexvmk87@mail.ru>
2020-02-27 16:18:57 -0800

    + skeleton for _4Sum

---
+++ LeetCode/Seattle/Sum/3SumClosest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Seattle.Sum
+{
+    public class _3SumClosest
+    {
+    }
+}
---
+++ LeetCode/Seattle/Sum/4Sum.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Aleksandr Rakushev (2020-present). All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Seattle
+{
+    /// <summary
+    /// Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target?
+    /// Find all unique quadruplets in the array which gives the sum of target.
+    /// Note:
+    /// The solution set must not contain duplicate quadruplets.
+    /// https://leetcode.com/problems/4sum/
+    /// </summary>
+    public class _4Sum
+    {
+
+    }
+
+    [Collection(SeattleCollectionFixture.SeattleCollectionFixtureName)]
+    public class PalindromeNumberTests
+    {
+        public PalindromeNumberTests(SeattleTestsFixture seattleTestsFixture)
+        {
+        }
+
+        [Fact]
+        public void IsPalindrome()
+        {
+
+        }
+    }
+}
+
