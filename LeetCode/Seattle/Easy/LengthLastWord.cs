87474121027eb58c5f4be7805ce9e57191755f46
Aleksandr Rakushev <alexvmk87@mail.ru>
2020-05-24 15:43:29 -0700

    * style

--- LeetCode/Seattle/Easy/LengthLastWord.cs
+++ LeetCode/Seattle/Easy/LengthLastWord.cs
@@ -19,17 +19,20 @@ namespace Seattle.Easy
             while (p > 0)
             {
                 p--;
+
                 // we're in the middle of the last word
                 if (s[p] != ' ')
                 {
                     length++;
                 }
+
                 // here is the end of last word
                 else if (length > 0)
                 {
                     return length;
                 }
             }
+
             return length;
         }
     }
