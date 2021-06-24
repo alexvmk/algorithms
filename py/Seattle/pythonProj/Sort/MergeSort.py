ca11d8cb55ffeca40a0eefa44cd2b09a7197a90d
Aleksandr Rakushev <a.rakushev@gmail.com>
2021-06-23 22:33:17 -0700

    + MergeSort

---
+++ py/Seattle/pythonProj/Sort/MergeSort.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def Sort(nums: List[int]) -> List[int]:
+        return [0,1,2]
+
+    def Merge(list1: List[int], list2: List[int]) -> List[int]:
+        return [0,1,2]
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('PyCharm')
+
+    nums = [12, 11, 13, 5, 6, 7 ]
+    sln = Solution
+
+    print(sln.Sort(nums))
+
+    print('Finished')
