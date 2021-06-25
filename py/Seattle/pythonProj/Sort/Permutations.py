bcc2ecca103a671019731c48b667acb846bcb2a7
Aleksandr Rakushev <a.rakushev@gmail.com>
2021-06-24 20:38:29 -0700

    * edits

--- py/Seattle/pythonProj/Sort/Permutations.py
+++ py/Seattle/pythonProj/Sort/Permutations.py
@@ -2,16 +2,21 @@ from typing import List
 
 class Solution:
     def permutate(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
         def backtrack(self, first):
+            # if all integers are used up
             if first == n:
                 output.append(nums[:])
             else:
                 for i in range(first,n):
+                    # place i-th integer first
+                    # in the current permutation
                     nums[first], nums[i] = nums[i], nums[first]
+                    # use next integers to complete the permutations
                     backtrack(self, first + 1)
+                    # backtrack
                     nums[first], nums[i] = nums[i], nums[first]
 
+        n = len(nums)
         output = []
         backtrack(self, 0)
         return output
